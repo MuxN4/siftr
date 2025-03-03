a1136c61783dd0284cf7c3ea858ea78f3de8ef75
MuxN4 <stev3npy@gmail.com>
2025-03-03 14:04:27 +0300

    implement HTTP server with CORS and health check endpoint

--- main.go
+++ main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 )
 
@@ -18,5 +21,32 @@ func main() {
 	if portString == "" {
 		log.Fatal("Missing PORT")
 	}
-	fmt.Println("Port:", portString)
+
+	router := chi.NewRouter()
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
+	// Health Router (Subrouter for Readiness)
+	healthRouter := chi.NewRouter()
+	healthRouter.Get("/ready", readinessHandler)
+
+	router.Mount("/health", healthRouter)
+
+	server := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
+	fmt.Println("Server running on port", portString)
+
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatal(err)
+	}
 }
