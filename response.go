1aaf38921987026fa7b8336bf5ae38e69bcf4217
MuxN4 <stev3npy@gmail.com>
2025-03-04 13:27:41 +0300

    add error response handling with logging for server errors

--- response.go
+++ response.go
@@ -2,8 +2,22 @@ package main
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
 )
+
+type ErrorResponse struct {
+	Message string `json:"message"`
+}
+
+func sendErrorResponse(w http.ResponseWriter, code int, message string) {
+	if code >= 500 {
+		log.Printf("Server error (%d): %s", code, message)
+	}
+
+	response := ErrorResponse{Message: message}
+	sendJSONResponse(w, code, response)
+}
 
 func sendJSONResponse(w http.ResponseWriter, code int, payload interface{}) {
 	data, err := json.Marshal(payload)
