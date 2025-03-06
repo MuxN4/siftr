0c6e052ca030f898d9b88e17199d438a8f8e2df2
MuxN4 <stev3npy@gmail.com>
2025-03-06 15:20:42 +0300

    implement create user handler with JSON parsing and error handling

---
+++ user_handler.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/MuxN4/siftr/internal/db"
+	"github.com/google/uuid"
+)
+
+func (apiCfg *apiConfig) createUserHandler(w http.ResponseWriter, r *http.Request) {
+
+	type Parameter struct {
+		Name string `json:"name"`
+	}
+	decoder := json.NewDecoder(r.Body)
+
+	params := Parameter{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		sendErrorResponse(w, 400, fmt.Sprintf("Error parsing JSON: %v", err))
+		return
+	}
+
+	user, err := apiCfg.DB.CreateUser(r.Context(), db.CreateUserParams{
+		ID:   uuid.New(),
+		Name: params.Name,
+	})
+	if err != nil {
+		sendErrorResponse(w, 400, fmt.Sprintf("Error creating user: %v", err))
+		return
+	}
+
+	sendJSONResponse(w, 200, databaseUserToUser(user))
+}
