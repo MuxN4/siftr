0ac8ff5ea3a0f0705a3c4ceaf157dc286c4e6eff
MuxN4 <stev3npy@gmail.com>
2025-03-18 12:52:08 +0300

    add GetUserByApiKey query to users.sql

--- database/queries/users.sql
+++ database/queries/users.sql
@@ -2,3 +2,6 @@
 INSERT INTO users (id, name, created_at, updated_at, api_key)
 VALUES ($1, $2, DEFAULT, DEFAULT, DEFAULT)
 RETURNING *;
+
+-- name: GetUserByApiKey :one
+SELECT * FROM users WHERE api_key = $1;
