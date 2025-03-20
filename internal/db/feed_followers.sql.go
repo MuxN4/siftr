83a4b34e6370d9599507a5c1d8e53889c7022e98
MuxN4 <stev3npy@gmail.com>
2025-03-20 13:55:25 +0300

    add SQL queries and handlers for retrieving and deleting feed followers

--- internal/db/feed_followers.sql.go
+++ internal/db/feed_followers.sql.go
@@ -35,3 +35,50 @@ func (q *Queries) CreateFeedFollowers(ctx context.Context, arg CreateFeedFollowe
 	)
 	return i, err
 }
+
+const deleteFeedFollowers = `-- name: DeleteFeedFollowers :exec
+DELETE FROM feed_followers WHERE id=$1 AND user_id=$2
+`
+
+type DeleteFeedFollowersParams struct {
+	ID     uuid.UUID
+	UserID uuid.UUID
+}
+
+func (q *Queries) DeleteFeedFollowers(ctx context.Context, arg DeleteFeedFollowersParams) error {
+	_, err := q.db.ExecContext(ctx, deleteFeedFollowers, arg.ID, arg.UserID)
+	return err
+}
+
+const getFeedFollowers = `-- name: GetFeedFollowers :many
+SELECT id, created_at, updated_at, user_id, feed_id FROM feed_followers WHERE user_id=$1
+`
+
+func (q *Queries) GetFeedFollowers(ctx context.Context, userID uuid.UUID) ([]FeedFollower, error) {
+	rows, err := q.db.QueryContext(ctx, getFeedFollowers, userID)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var items []FeedFollower
+	for rows.Next() {
+		var i FeedFollower
+		if err := rows.Scan(
+			&i.ID,
+			&i.CreatedAt,
+			&i.UpdatedAt,
+			&i.UserID,
+			&i.FeedID,
+		); err != nil {
+			return nil, err
+		}
+		items = append(items, i)
+	}
+	if err := rows.Close(); err != nil {
+		return nil, err
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	return items, nil
+}
