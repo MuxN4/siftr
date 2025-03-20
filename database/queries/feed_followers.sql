99566f9f9efa6bf4c365709286360516a906bc78
MuxN4 <stev3npy@gmail.com>
2025-03-20 13:54:53 +0300

    add SQL queries for retrieving and deleting feed followers

--- database/queries/feed_followers.sql
+++ database/queries/feed_followers.sql
@@ -3,3 +3,8 @@ INSERT INTO feed_followers (id, created_at, updated_at, user_id, feed_id)
 VALUES ($1, DEFAULT, DEFAULT, $2, $3)
 RETURNING *;
 
+-- name: GetFeedFollowers :many
+SELECT * FROM feed_followers WHERE user_id=$1;
+
+-- name: DeleteFeedFollowers :exec
+DELETE FROM feed_followers WHERE id=$1 AND user_id=$2;
