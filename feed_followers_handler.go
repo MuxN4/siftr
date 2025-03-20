9bc7cd26b3cdd5e3d253db4195f5af564a6519f5
MuxN4 <stev3npy@gmail.com>
2025-03-20 13:56:06 +0300

    include handlers for retrieving and deleting feed followers

--- feed_followers_handler.go
+++ feed_followers_handler.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 
 	"github.com/MuxN4/siftr/internal/db"
+	"github.com/go-chi/chi/v5"
 	"github.com/google/uuid"
 )
 
@@ -34,4 +35,36 @@ func (apiCfg *apiConfig) createFeedFollowersHandler(w http.ResponseWriter, r *ht
 	}
 
 	sendJSONResponse(w, 201, databaseFeedFollowersToFeedFollowers(FeedFollowers))
+}
+
+func (apiCfg *apiConfig) getFeedFollowersHandler(w http.ResponseWriter, r *http.Request, user db.User) {
+	FeedFollowers, err := apiCfg.DB.GetFeedFollowers(r.Context(), user.ID)
+	if err != nil {
+		sendErrorResponse(w, 400, fmt.Sprintf("Error getting feed followers: %v", err))
+		return
+	}
+
+	sendJSONResponse(w, 201, databaseFeedsFollowersToFeedsFollowers(FeedFollowers))
+}
+
+func (apiCfg *apiConfig) deleteFeedFollowersHandler(w http.ResponseWriter, r *http.Request, user db.User) {
+	feedFollowIDStr := chi.URLParam(r, "feedFollowID")
+	feedFollowID, err := uuid.Parse(feedFollowIDStr)
+	if err != nil {
+		sendErrorResponse(w, 400, fmt.Sprintf("Error parsing feed follow ID: %v", err))
+		return
+	}
+
+	err = apiCfg.DB.DeleteFeedFollowers(r.Context(), db.DeleteFeedFollowersParams{
+		ID:     feedFollowID,
+		UserID: user.ID,
+	})
+
+	if err != nil {
+		sendErrorResponse(w, 400, fmt.Sprintf("Error deleting feed followers: %v", err))
+		return
+	}
+
+	sendJSONResponse(w, 200, struct{}{})
+
 }
